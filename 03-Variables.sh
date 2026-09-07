f29235ed73ebc0a6f4792df2199514032cf47361
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-08 02:52:44 +0530

    “message”

---
+++ 03-Variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Person1=Prassu
+Person2=Deepu
+
+
+
+echo "$Person1: Hello $Person2, how are you doing?"
+echo "$Person2: Hi $Person1, I'm good."
+echo "$Person1: I'm going to outing. Will you come with me?"
+echo "$Person2: Yes, I will come but at what time and date?"
+echo "$Person1: I will let you know soon. Thank you!"
