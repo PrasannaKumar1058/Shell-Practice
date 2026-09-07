3fa776879aa683f1317135e61972f3008dcc2378
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-08 02:46:12 +0530

    “message”

--- 02-Conversation.sh
+++ 02-Conversation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+Person1=Prassu
+Person2=Deepu
+
+
+
 echo "Prassu: Hello Deepu, how are you doing?"
 echo "Deepu: Hi Prassu, I'm good."
 echo "Prassu: I'm going to outing. Will you come with me?"
