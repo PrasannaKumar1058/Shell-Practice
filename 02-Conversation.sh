5396fc2d71fa007fd2f6a01fa8604139e7a61ebf
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-08 02:36:50 +0530

    “message”

---
+++ 02-Conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Prassu: Hello Deepu, how are you doing?"
+echo "Deepu: Hi Prassu, I'm good."
+echo "Prassu: I'm going to outing. Will you come with me?"
+Echo "Deepu: Yes, I will come but at what time and date?"
+Echo "Prassu: I will let you know soon. Thank you!"
