3209ff44e4beff009d9b785b8004a21e41d2df6f
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-08 04:00:45 +0530

    “message”

---
+++ 07-Array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("HIT 3" "COURT" "TANDEL" "RANGAM")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+echo "Fourth Movie: ${MOVIES[3]}"
+
+echo "All Movies: ${MOVIES[@]}"
+
