461756d1786e50affafb668b0a608920c1ee1d19
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-12 04:00:46 +0530

    “message”

--- 10-Install-Commands.sh
+++ 10-Install-Commands.sh
@@ -12,6 +12,11 @@ echo "You are runnig with root privilages"
 
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+echo "MySQL is not installed. Installing the MySQL package in your system"
 dnf install mysql -y
 
 if [ $? -eq 0 ]
@@ -24,3 +29,59 @@ echo "Installing mysql is failure. Please check the error and try again"
 exit 1
 
 fi
+
+else
+echo "MySQL is already installed in your system"
+
+fi 
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+# echo "Installing mysql is success"
+
+# else
+
+# echo "Installing mysql is failure. Please check the error and try again"
+# exit 1
+
+# fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# To check if the package is installed or not, you can use the following command:
+# dnf list installed mysql
