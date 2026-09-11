a5d1c4ebda47b90e2942cff0ff64698dd2c16567
prasannakumar <prasanna.kumar547@gmail.com>
2026-09-12 03:22:21 +0530

    “message”

--- 10-Install-Commands.sh
+++ 10-Install-Commands.sh
@@ -5,8 +5,22 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "Please run this script with root privilages"
+exit 1
 
 else
 echo "You are runnig with root privilages"
+
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+echo "Installing mysql is success"
+
+else
+
+echo "Installing mysql is failure. Please check the error and try again"
+exit 1
 
 fi
